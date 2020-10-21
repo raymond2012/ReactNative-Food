bb5ad5984136f39582215704dd74f8d8bf0eb7d9
Raymond <raymond96521>
2020-10-21 18:34:45 +0800

    Create Searcch result list

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,29 +2,12 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get("/search", {
-        params: { limit: 50, term: searchTerm, location: "san jose" },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage("Something went wrong");
-    }
-  };
-
-  // Call searchApi when component
-  // is first rendered. BAD CODE!
-  // searchApi("pasta");
-  useEffect(() => {
-    searchApi("pasta");
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
@@ -35,6 +18,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList title="Cost Effective" />
+      <ResultsList title="Bit Pricer" />
+      <ResultsList title="Big Spender!" />
     </View>
   );
 };
